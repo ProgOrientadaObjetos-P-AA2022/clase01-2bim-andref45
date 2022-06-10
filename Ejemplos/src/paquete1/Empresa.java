a33856c3d6e765a7d4550b3875f979f2d71475d8
André Flores <oaflores7@utpl.edu.ec>
2022-06-10 11:28:48 -0500

    Cambio

--- Ejemplos/src/paquete1/Empresa.java
+++ Ejemplos/src/paquete1/Empresa.java
@@ -10,46 +10,53 @@ package paquete1;
  * @author reroes
  */
 public class Empresa {
+
     private String nombre;
-    private Edificio [] edificios;
+    private Edificio[] edificios;
     private double costoBienesInmuebles;
-    
-    public void establecerNombre(String m){
+
+    public void establecerNombre(String m) {
         nombre = m;
     }
-    
-    public void establecerEdificios(Edificio[] m){
+
+    public void establecerEdificios(Edificio[] m) {
         edificios = m;
     }
-    public void establecerCostoBienesInmuebles(){
+
+    public void establecerCostoBienesInmuebles() {
         double suma = 0;
         for (int i = 0; i < edificios.length; i++) {
             suma += edificios[i].obtenerCosto();
         }
         costoBienesInmuebles = suma;
     }
-    public String obtenerNombre(){
+
+    public String obtenerNombre() {
         return nombre;
     }
-    
-    public Edificio[] obtenerEdificios(){
+
+    public Edificio[] obtenerEdificios() {
         return edificios;
     }
-    public double obtenerCostoBienesInmuebles(){
+
+    public double obtenerCostoBienesInmuebles() {
         return costoBienesInmuebles;
     }
-    
-    @Override 
-    public String toString(){
-        String cadena = String.format("%s\nLista de edificios:\n", 
+
+    @Override
+    public String toString() {
+        String cadena = String.format("%s\nLista de edificios:\n",
                 nombre);
         for (int i = 0; i < edificios.length; i++) {
-            cadena = String.format("%s%d. %s (%.0f)\n", 
-                    cadena,(i+1), edificios[i].obtenerNombre().toUpperCase(),
+            cadena = String.format("%s%d. %s (%.0f)\n",
+                    cadena,
+                    i + 1,
+                    edificios[i].obtenerNombre().toUpperCase(),
                     edificios[i].obtenerCosto());
         }
         cadena = String.format("%sTotal de inmuebles: %.0f",
-                cadena, costoBienesInmuebles);
+                cadena,
+                costoBienesInmuebles);
         return cadena;
     }
 }
